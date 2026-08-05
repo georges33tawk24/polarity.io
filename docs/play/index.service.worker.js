b413168d01f858daa2d6d2b064b19e407ee860b1
Georges Tawk <georges33tawk24@gmail.com>
2026-08-05 10:00:49 +0300

    Rebuild the web build with the repel fix

--- docs/play/index.service.worker.js
+++ docs/play/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785887157|5581875';
+const CACHE_VERSION = '1785913246|8001350';
 /** @type {string} */
 const CACHE_PREFIX = 'Polarity.io-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
